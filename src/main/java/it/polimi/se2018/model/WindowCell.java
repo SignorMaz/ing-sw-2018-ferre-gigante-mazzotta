ee5f3cc534c17d401fbbbf2f35cb88bafb9d6e16
DoraGiga <dora.gigante@mail.polimi.it>
2018-05-29 16:33:21 +0200

    Add window pattern load test

--- src/main/java/it/polimi/se2018/model/WindowCell.java
+++ src/main/java/it/polimi/se2018/model/WindowCell.java
@@ -1,6 +1,7 @@
 package it.polimi.se2018.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class WindowCell implements Serializable {
     public final Color color;
@@ -9,5 +10,19 @@ public class WindowCell implements Serializable {
     public WindowCell(Color color, int number) {
         this.color = color;
         this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WindowCell that = (WindowCell) o;
+        return number == that.number &&
+                color == that.color;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, number);
     }
 }
