ff9742784ee54bf37b43fc634f8e5483e3d6b5df
marcoferre <10467480@polimi.it>
2018-06-22 17:50:57 +0200

    Print some CLI messages

--- src/main/java/it/polimi/se2018/view/cli/PlayerViewCli.java
+++ src/main/java/it/polimi/se2018/view/cli/PlayerViewCli.java
@@ -28,52 +28,59 @@ public class PlayerViewCli implements PlayerView {
 
     @Override
     public void onLogin(boolean result) {
-
+        System.out.println("You logged in");
     }
 
     @Override
     public void onInitialSetup(InitialSetupEvent.Data data) {
-
     }
 
     @Override
     public void onDicePlaced(String playerPlacingDice, Position position, Dice dice) {
-
+        if (playerPlacingDice.equals(getPlayerViewBase().getPlayerId())) {
+            System.out.println("You successfully placed the dice");
+        } else {
+            System.out.println(playerPlacingDice + "'s placed a dice");
+        }
     }
 
     @Override
     public void onDraftPoolChanged(List<Dice> draftPool) {
-        
+        System.out.println("The draft pool changed");
     }
 
     @Override
     public void onGameOver() {
-
+        System.out.println("The game is over");
     }
 
     @Override
     public void onInvalidAction(Action action) {
-
+        System.out.println("Invalid move");
     }
 
     @Override
     public void onNewTurn(String playerId) {
-
+        if (getPlayerViewBase().getPlayerId().equals(playerId)) {
+            System.out.println("It's your turn");
+        } else {
+            System.out.println("It's " + playerId + "'s turn!");
+        }
+        // TODO: Enable/disable user input
     }
 
     @Override
     public void onPointsChanged(int points) {
-
     }
 
     @Override
     public void onTokensChanged(int tokens) {
-
+        System.out.println("Your new tokens count is " + getPlayerViewBase().getFavorTokens());
     }
 
     @Override
     public void onGameStarted(Map<String, WindowPattern> windowPatternMap) {
-
+        System.out.println("Game started!");
     }
 
     public static void main(String[] args) throws IOException, NotBoundException {
@@ -89,6 +96,10 @@ public class PlayerViewCli implements PlayerView {
 
         PlayerView view = new PlayerViewCli(playerId, type);
         System.out.println("Connecting...");
-        view.getPlayerViewBase().login();
+        try {
+            view.getPlayerViewBase().login();
+        } catch (IOException exception) {
+            System.out.println("Could not login. Please try again witha different username");
+        }
     }
 }
