5e2f9dcf5f77268fec3e49a5065f6e5b5ac6c15a
Maurizio Mazzotta <maurizio.mazzotta@mail.polimi.it>
2018-06-25 15:41:49 +0200

    Add method that puts the WindowFrame into a String

--- src/main/java/it/polimi/se2018/view/cli/WindowFramePrinter.java
+++ src/main/java/it/polimi/se2018/view/cli/WindowFramePrinter.java
@@ -7,44 +7,56 @@ public class WindowFramePrinter {
     private WindowFramePrinter() {
     }
 
-    private static void printCellRow(WindowFrame windowFrame, int row) {
+    private static String cellRowToString(WindowFrame windowFrame, int row) {
+        StringBuilder sb = new StringBuilder();
         String missingElementChar = "-";
-        System.out.print("│");
+        sb.append("│");
         for (int j = 0; j < WindowPattern.COLUMNS; j++) {
             Position curPosition = new Position(row, j);
             WindowCell cell = windowFrame.getWindowPattern().getPattern().get(curPosition);
             if (cell.number == 0) {
-                System.out.print(missingElementChar);
+                sb.append(missingElementChar);
             } else {
-                System.out.print(cell.number);
+                sb.append(cell.number);
             }
             if (cell.color == Color.BLANK) {
-                System.out.print(missingElementChar);
+                sb.append(missingElementChar);
             } else {
-                System.out.print(cell.color.toString().charAt(0));
+                sb.append(cell.color.toString().charAt(0));
             }
-            System.out.print("│");
+            sb.append("│");
         }
-        System.out.println();
+        sb.append("\n");
+
+        return sb.toString();
     }
 
-    private static void printDiceRow(WindowFrame windowFrame, int row) {
-        System.out.print("│");
+    private static String diceRowToString(WindowFrame windowFrame, int row) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("│");
         for (int j = 0; j < WindowPattern.COLUMNS; j++) {
             Position curPosition = new Position(row, j);
             Dice dice = windowFrame.getPlacedDices().get(curPosition);
             if (dice != null) {
-                System.out.print(dice.getNumber());
-                System.out.print(dice.getColor().toString().charAt(0));
+                sb.append(dice.getNumber());
+                sb.append(dice.getColor().toString().charAt(0));
             } else {
-                System.out.print("  ");
+                sb.append("  ");
             }
-            System.out.print("│");
+            sb.append("│");
         }
-        System.out.println();
+        sb.append("\n");
+
+        return sb.toString();
     }
 
     public static void print(WindowFrame windowFrame) {
+        System.out.print(frameToString(windowFrame));
+    }
+
+    public static String frameToString(WindowFrame windowFrame) {
+        StringBuilder sb = new StringBuilder();
+
         /*
          * First row of each cell: WindowCell
          * Second row of each cell: Dice
@@ -66,48 +78,56 @@ public class WindowFramePrinter {
          */
         String rowNumberFormat = "%4s";
 
-        System.out.println("Map: " + windowFrame.getWindowPattern().getName());
-        System.out.println("Difficulty: " + windowFrame.getWindowPattern().getDifficulty());
-        System.out.println();
+        sb.append("Map: ");
+        sb.append(windowFrame.getWindowPattern().getName());
+        sb.append("\n");
+        sb.append("Difficulty: ");
+        sb.append(windowFrame.getWindowPattern().getDifficulty());
+        sb.append("\n\n");
 
         String lefMargin = String.format(rowNumberFormat, "");
 
-        System.out.print(lefMargin);
+        sb.append(lefMargin);
         for (int j = 0; j < WindowPattern.COLUMNS; j++) {
-            System.out.print(String.format(" %d ", j + 1));
+            sb.append(String.format(" %d ", j + 1));
         }
-        System.out.println();
+        sb.append("\n");
 
         // First row
-        System.out.print(String.format("%s┌──┬", lefMargin));
+        sb.append(String.format("%s┌──┬", lefMargin));
         for (int j = 1; j < WindowPattern.COLUMNS - 1; j++) {
-            System.out.print("──┬");
+            sb.append("──┬");
         }
-        System.out.println("──┐");
+        sb.append("──┐");
+        sb.append("\n");
 
-        System.out.print(String.format(rowNumberFormat, 1));
-        printCellRow(windowFrame, 0);
-        System.out.print(lefMargin);
-        printDiceRow(windowFrame, 0);
+        sb.append(String.format(rowNumberFormat, 1));
+        sb.append(cellRowToString(windowFrame, 0));
+        sb.append(lefMargin);
+        sb.append(diceRowToString(windowFrame, 0));
 
         for (int i = 1; i < WindowPattern.ROWS; i++) {
-            System.out.print(String.format("%s├──┼", lefMargin));
+            sb.append(String.format("%s├──┼", lefMargin));
             for (int j = 1; j < WindowPattern.COLUMNS - 1; j++) {
-                System.out.print("──┼");
+                sb.append("──┼");
             }
-            System.out.println("──┤");
+            sb.append("──┤");
+            sb.append("\n");
 
-            System.out.print(String.format(rowNumberFormat, i));
-            printCellRow(windowFrame, i);
-            System.out.print(lefMargin);
-            printDiceRow(windowFrame, i);
+            sb.append(String.format(rowNumberFormat, i));
+            sb.append(cellRowToString(windowFrame, i));
+            sb.append(lefMargin);
+            sb.append(diceRowToString(windowFrame, i));
         }
 
         // Last row
-        System.out.print(String.format("%s└──┴", lefMargin));
+        sb.append(String.format("%s└──┴", lefMargin));
         for (int j = 1; j < WindowPattern.COLUMNS - 1; j++) {
-            System.out.print("──┴");
+            sb.append("──┴");
         }
-        System.out.println("──┘");
+        sb.append("──┘");
+        sb.append("\n");
+
+        return sb.toString();
     }
 }
