4730d9ce02b3a2250d90a50dbacb261e2613640c
Maurizio Mazzotta <maurizio.mazzotta@mail.polimi.it>
2018-07-01 01:18:32 +0200

    Update WindowFrameTest

--- src/test/java/it/polimi/se2018/model/WindowFrameTest.java
+++ src/test/java/it/polimi/se2018/model/WindowFrameTest.java
@@ -1,4 +1,4 @@
-/*package it.polimi.se2018.model;
+package it.polimi.se2018.model;
 
 import org.junit.Test;
 
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class WindowFrameTest {
 
@@ -69,24 +70,24 @@ public class WindowFrameTest {
         assertTrue(windowFrame.isPositionValid(new Dice(Color.GREEN), new Position(3, 0), toolCard));
         assertTrue(windowFrame.isPositionValid(new Dice(Color.PURPLE), new Position(0, 1), toolCard));
         assertTrue(windowFrame.isPositionValid(new Dice(Color.BLUE), new Position(3, 1), toolCard));
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.BLUE), new Position(2, 3), toolCard));
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.RED, 2), new Position(0, 2), toolCard));     //Wrong number
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.RED, 3), new Position(3, 4), toolCard));
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.YELLOW), new Position(2, 3), toolCard));         //Corner
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.BLUE), new Position(3, 0), toolCard));          //Color not valid
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.RED), new Position(2, 0), toolCard));
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.YELLOW), new Position(2, 0), toolCard));
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.GREEN), new Position(1, 4), toolCard));
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.GREEN), new Position(-1, 4), toolCard));        // The position doesn't exist
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.GREEN), new Position(1, 5), toolCard));
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.GREEN), new Position(4, -4), toolCard));
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.BLUE), new Position(2, 3), toolCard));
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.RED, 2), new Position(0, 2), toolCard));     //Wrong number
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.RED, 3), new Position(3, 4), toolCard));
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.YELLOW), new Position(2, 3), toolCard));         //Corner
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.BLUE), new Position(3, 0), toolCard));          //Color not valid
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.RED), new Position(2, 0), toolCard));
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.YELLOW), new Position(2, 0), toolCard));
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.GREEN), new Position(1, 4), toolCard));
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.GREEN), new Position(-1, 4), toolCard));        // The position doesn't exist
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.GREEN), new Position(1, 5), toolCard));
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.GREEN), new Position(4, -4), toolCard));
         windowFrame.placeDice(new Dice(Color.RED, 6), new Position(0, 0), toolCard);
-        assertTrue(windowFrame.isPositionValid(new Dice(Color.BLUE, 5), new Position(1, 0), toolCard));
-        assertTrue(windowFrame.isPositionValid(new Dice(Color.BLUE, 5), new Position(1, 1), toolCard));
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.RED, 6), new Position(0, 0), toolCard));        // The position is already occupied
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.BLUE), new Position(2, 0), toolCard));                // Every dice must be adjacent to an already placed dice
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.RED), new Position(1, 0), toolCard));       // Orthogonally adjacent dices must have different color
-        //assertFalse(windowFrame.isPositionValid(new Dice(Color.BLUE, 6), new Position(1, 0), toolCard));       // Orthogonally adjacent dices must have different number
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.BLUE, 5), new Position(1, 0), toolCard));
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.BLUE, 5), new Position(1, 1), toolCard));
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.RED, 6), new Position(0, 0), toolCard));        // The position is already occupied
+        assertTrue(windowFrame.isPositionValid(new Dice(Color.BLUE), new Position(2, 0), toolCard));                // Every dice must be adjacent to an already placed dice
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.RED), new Position(1, 0), toolCard));       // Orthogonally adjacent dices must have different color
+        assertFalse(windowFrame.isPositionValid(new Dice(Color.BLUE, 6), new Position(1, 0), toolCard));       // Orthogonally adjacent dices must have different number
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -94,9 +95,8 @@ public class WindowFrameTest {
         ToolCard toolCard = null;
         WindowPattern windowPattern = new WindowPatternTest();
         WindowFrame windowFrame = new WindowFrame(windowPattern);
-        windowFrame.placeDice(new Dice(Color.RED, 6), new Position(0, 0), toolCard);
+        windowFrame.placeDice(new Dice(Color.BLUE, 6), new Position(0, 0), toolCard);
     }
 
 }
 
-*/
